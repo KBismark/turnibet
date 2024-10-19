9fd85c56fa2416706e2223fbe6295e9d95264f1d
Bismark Yamoah <bismarkkwabenayamoah@gmail.com>
2024-10-19 19:36:32 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const { app } = require('./app');
-
+app.listen(()=>{});
+/*
 app.listen(6097, ()=>{
     console.log('Server running at port '+ 6097);
     console.log('See http://localhost:'+ 6097);
 })
 
+*/
